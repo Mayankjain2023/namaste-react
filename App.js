f564f114b47ab98506877c29721fc878e185c69a
Mayank Jain <mayankjain@Mayanks-MacBook-Air.local>
2023-12-03 21:30:48 +0530

    ep-4-done

--- App.js
+++ App.js
@@ -54,15 +54,104 @@ const Header = () =>{
  */
 
 const RestoCard = (props)=>{
-    const {resName,rate,rating } = props;
+    const {resData } = props;
+    console.log(resData);
+    const {name,cuisine,rating,phone} = resData
+
     return (
         <div className="res-card">
             <img className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e33e1d3ba7d6b2bb0d45e1001b731fcf" />
-            <h3>{resName}</h3>
-            <h4>Rate : {rate} </h4>
+            <h3>{name}</h3>
+            <h4>{cuisine}</h4>
+            <h4>Rate : {rating} </h4>
+            <span>
+                Phone : {phone}
+            </span>
         </div>
     )
 }
+
+
+ const resList =  [
+        {
+          "id": 1,
+          "name": "Delicious Delights",
+          "cuisine": "Italian",
+          "address": "123 Main Street, Cityville",
+          "phone": "+1 (555) 123-4567",
+          "image": "https://example.com/delicious_delights.jpg",
+          "rating": 4.5,
+          "menu": [
+            {
+              "id": 101,
+              "name": "Margherita Pizza",
+              "price": 12.99
+            },
+            {
+              "id": 102,
+              "name": "Pasta Carbonara",
+              "price": 15.99
+            },
+            {
+              "id": 103,
+              "name": "Tiramisu",
+              "price": 8.99
+            }
+          ]
+        },
+        {
+          "id": 2,
+          "name": "Spicy Bites",
+          "cuisine": "Mexican",
+          "address": "456 Oak Avenue, Townsville",
+          "phone": "+1 (555) 987-6543",
+          "image": "https://example.com/spicy_bites.jpg",
+          "rating": 4.2,
+          "menu": [
+            {
+              "id": 201,
+              "name": "Tacos",
+              "price": 9.99
+            },
+            {
+              "id": 202,
+              "name": "Burrito Bowl",
+              "price": 11.99
+            },
+            {
+              "id": 203,
+              "name": "Churros",
+              "price": 6.99
+            }
+          ]
+        },
+        {
+          "id": 3,
+          "name": "Sushi Haven",
+          "cuisine": "Japanese",
+          "address": "789 Pine Street, Villagetown",
+          "phone": "+1 (555) 234-5678",
+          "image": "https://example.com/sushi_haven.jpg",
+          "rating": 4.8,
+          "menu": [
+            {
+              "id": 301,
+              "name": "Sashimi Platter",
+              "price": 18.99
+            },
+            {
+              "id": 302,
+              "name": "Dragon Roll",
+              "price": 14.99
+            },
+            {
+              "id": 303,
+              "name": "Green Tea Ice Cream",
+              "price": 7.99
+            }
+          ]
+        }
+      ]
 
 const Body = () =>{
     return (
@@ -70,38 +159,13 @@ const Body = () =>{
             <div className="search">Search</div>
 
                 <div className="res-container">
-                        <RestoCard 
-                        resName = "Outlet 1" 
-                        rate="300"
-                        rating='5'
-                        />
-                        <RestoCard
-                        resName = "Outlet 2"
-                        rate="340"
-                        rating='4.5'
-                        />
-                        <RestoCard 
-                        resName = "Outlet 3" 
-                        rate="560"
-                        rating='4.8'
-
-                        />
-                        <RestoCard
-                        resName = "Outlet 4"
-                        rate="60"
-                        rating='4'
-                        />
-                        <RestoCard 
-                        resName = "Outlet 5" 
-                        rate="270"
-                        rating='3.9'
-                        />
-                        <RestoCard 
-                        resName = "Outlet 6" 
-                        rate="45"
-                        rating='4.7'
-                        />
-                        
+                    {
+                        //always give a key when looping and passing it to props as each prop must be uniquely represented
+                        //don't use index as keys
+                        resList.map((res) => (
+                        <RestoCard key={res.id} resData = {res}/>
+                        ))
+                    }
 
                 </div>
         </div>
