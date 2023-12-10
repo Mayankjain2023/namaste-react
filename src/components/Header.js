8ff2f2ae1b9d19af1c7300fa5eca8e3c17dbce94
Mayank Jain <mayankjain@Mayanks-MacBook-Air.local>
2023-12-11 01:26:31 +0530

    till-1.22

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
+import { useState } from "react"
 import { LOGO_URL } from "../utils/constants"
 
+
+
+
 const Header = () =>{
+    let btnName = "Logout";
+
+
+    const [loginState,setLoginState] = useState('Logout');
+
     return (
+
+     
         <div className="heading">
             <div className="logo-container">
                 <img className= "logo" src={LOGO_URL}/>
@@ -12,6 +23,12 @@ const Header = () =>{
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+
+                    <span >
+                        <button className="login-button" onClick={()=>{console.log("click"); loginState == 'Login' ? setLoginState('Logout') : setLoginState('Login')}}>
+                          {loginState}
+                        </button>
+                    </span>
                     
                 </ul>
             </div>    
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,3 @@
 export const LOGO_URL = "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
+export const IMAGE_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const dummy_url = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e33e1d3ba7d6b2bb0d45e1001b731fcf";
