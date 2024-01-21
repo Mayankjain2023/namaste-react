6e38d2dbc5dc1e9654accea68c547ef4c10469b4
Mayank Jain <mayankjain@Mayanks-MacBook-Air.local>
2024-01-21 15:55:29 +0530

    done

--- src/components/About.js
+++ src/components/About.js
@@ -31,4 +31,60 @@ componentDidMount(){
 }
 
 
+
+/*
+     When there are multiple child compoenents having componentDidMount in them then the way their lifecyle works changes completely 
+     in order to optimize the performance
+
+     expected execution ? =>{
+        ->parent constructor
+        ->parent render
+            ->child constructor
+            ->child render
+            ->child component didmount
+
+            ->child constructor 
+            ->child render
+            ->child component didmount
+
+         ->parent component didmount
+
+     } -> BUT THIS IS NOT THE WAY IT WORKS
+
+
+     IN ORDER THE OPTIMIZE THE PERFORMANCE REACT EXECUTES 
+     -> component did mount after all the child component has rendered , sequentially only after all the child components have
+     rendered the their component did mount is called ,after the child component did mount has executed successfully for all childs then 
+     the parents component did mount executes
+
+
+     -As in react life cycle first Render() phase happens then the Commit () phase
+     1) Rende() -> clubbes for all the child components 
+     2) Commit() -> (Component did mount is executed in this commit)
+
+
+      actual execution ? =>{
+        ->parent constructor
+        ->parent render
+            ->child constructor
+            ->child render
+
+            ->child constructor 
+            ->child render
+
+            ->child 1 component didmount
+            ->child 2 component didmount
+        ->parent component didmount
+
+     }
+
+
+     Why react batches render method for all the components ?
+     -> Because once the commit phase starts , react starts UPDATING the DOM , and the DOM manipulation is the most expensive operation
+     so it executes in batch the render process as well as the commit methods together so that DOM updation happens once for all
+
+*/
+
+
+
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -13,42 +13,54 @@ class UserClass extends React.Component{
 
     constructor(props){
         super(props)
-
-
         this.state = {
-            count:0
+            userInfo : {
+                name:"Mayank",
+                location: "INDIA"
+            } 
         }
         console.log("child Constructor")
-        console.log("pr",props);
     }
 
     //Imp method provided by the react component class
     //once the component (constructor as well as render are called ) is mounted on the DOM after that this method is called
-    componentDidMount(){
+    async componentDidMount(){
             console.log("child Component mount");
-            
-
             //MAKE an api call over here and then fill the data in the component
+            const user = await fetch('https://api.github.com/users/Mayankjain2023');
+            const data = await user.json()
+            console.log("data user",data);
+
+                this.setState({
+                    userInfo:data
+                });
+            
+           
+
+    }
+
+    componentDidUpdate (){
+        console.log("Child component component did update")
+    }
+
+    componentWillUnmount(){
+        //after the component is removed/unmounted from the dom
+        console.log("child component will unmount")
     }
 
     render(){
         console.log("Rendering child  component")
+
+        const {name,location,avatar_url } = this.state.userInfo;
+    
         return (
             <div className="user-card">
                 <h2>This is a user card</h2>
-               
-                <p>Count : {this.state.count}</p>
-                <button onClick={()=>{
-                    // this.state.count+=1 do not update state variable directly by equating the value
-
-                    this.setState({
-                        count: this.state.count + 1
-                    })
-                }
-                    } >+</button>
-                <ul>
-                        <li>Name: {this.props.name}</li>
-                        <li>Location: {this.props.location}</li>
+                <ul>    <span>
+                            <img className="user-img" src={avatar_url}/>
+                        </span>
+                        <li>Name: {name}</li>
+                        <li>Location: {location}</li>
                 </ul>
 
             </div>
@@ -57,3 +69,60 @@ class UserClass extends React.Component{
 }
 
 export default UserClass;
+
+
+
+/**
+ * ---- MOUNTING CYCLE ---- 
+ * 
+    * Parent constructor
+    * parent render 
+    *      -> child constructor
+    *      -> child render
+    *      -> component didmount{
+    *          ->API CALL
+    *          ->this.setState() -> which updates the value in the DOM -> render() again
+    *          }
+ * ----- MOUNTING CYCLE Completes ---- 
+ *          
+ * After this componentDidUpdate() 
+ * When component is unmounted fromt the DOM then componentWillUnmount() is called 
+ * 
+ * ALL this used to be done with Class components -> 
+ * BUT with functional components we have useEffect , useState() etc to do the sane in the lesser lines of code
+ * 
+ * 
+ * 
+ * ComponentDidMount is not equivalent to useEffect () -> 
+ * useEffect does not use componentdid mount behind ht scenes
+ * 
+ * 
+ * 
+ * after using componentDidMount it is necessary to use componentWillUnmount() to unload it otherwise there will be multiple instances of the component running Behind the scene
+ */
+
+
+/**
+ * In case of useEffect(()=>{
+ * 
+ * },[])
+ * 
+ * 
+ * 
+ * it takes empty dependency array ,
+ * it makes api call only once during the initial render, i.e when the component loads for the first time
+ * 
+ * BUT if we use setInterval inside useEffect () we will find that while we move to the other components it is still executing so 
+ * we need to clean it up just line component did mount 
+ * we do this by returning from useEffect()
+ * 
+ * useEffect(()=>{
+    *  setInterval(()=>{
+    * },1000)
+ * 
+ * return ()=>{
+ *  console.log("Useffecy return")
+ * }
+ * },[])
+ * 
+ */
